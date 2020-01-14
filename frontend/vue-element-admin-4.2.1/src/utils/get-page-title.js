6b89a43836577e1ea3a44bd52c07543a105e2282
Ray Zhang <zhangruihuai@gmail.com>
2020-01-14 19:46:45 +0800

    change title

--- frontend/vue-element-admin-4.2.1/src/utils/get-page-title.js
+++ frontend/vue-element-admin-4.2.1/src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Element Admin'
+const title = defaultSettings.title || 'Equipment Management System'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
